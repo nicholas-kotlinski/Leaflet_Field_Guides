5420c5047514a22364833ae9c3b49a2edad0aaa5
nicholas-kotlinski <48604961+nicholas-kotlinski@users.noreply.github.com>
2019-04-02 15:38:55 -0500

    Add files via upload

---
+++ dashboard.R
@@ -0,0 +1,93 @@
+list.of.packages <- c("shiny", "shinydashboard", "raster", "rgdal", "sp", "leaflet", "geojsonio", "markdown", "ggplot2")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+library(shiny)
+library(shinydashboard)
+library(raster)
+library(rgdal)
+library(sp)
+library(leaflet)
+library(geojsonio)
+library(markdown)
+library(ggplot2)
+library(dplyr)
+
+fm <- geojsonio::geojson_read("data/fm_guides_r.geojson", what = "sp")
+fg.data <- read.csv("data/FG_cleaned_3_12_2019.csv")
+
+ui <- dashboardPage(
+  
+  dashboardHeader(title = "Field Guides Dashboard"),
+  
+  dashboardSidebar(title = "Controls",
+                   selectInput(inputId = "variableselected", label = "Select country", 
+                               choices = fm$admin),
+                   textOutput("selected_var")),
+                   #selectInput("cInput", "Select an Inventory", choices = unique(data$country)),
+                   #selectInput(inputId = "YInput", label = "Select Field Guides",
+                   #            choices = data$guide_no),
+                   
+                   #plotOutput("PieChart"),
+  
+  dashboardBody(
+    fluidPage(
+      tags$style(type = "text/css", "#map {height: calc(100vh - 20px) !important;}"),
+      leafletOutput("map")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+    
+  output$selected_var <- renderText({ 
+    paste("You have selected", input$variableselected)
+  })
+  
+  output$map <- renderLeaflet({
+
+    bins <- c(1, 5, 50, 250)
+    pal <- colorBin("Blues", domain = states$FG_Numb, bins = bins)
+    
+    labels <- sprintf(
+      "<strong>%s</strong><br/><strong>%g</strong> Field Guides<sup></sup><br>Link:<br/>",
+      fm$admin, fm$FG_Numb) %>% lapply(htmltools::HTML)
+    
+    m <- leaflet(fm, height = "100%", options = leafletOptions(minZoom = 1, maxZoom = 5)) %>%
+      setView(7, 17, 1.5) %>%
+      addTiles(group = "OSM") %>%
+      addProviderTiles(providers$Stamen.TonerBackground, group = "Black & White")
+
+    m %>% addPolygons(
+      fillColor = ~pal(FG_Numb),
+      weight = 1,
+      opacity = 1,
+      color = "black",
+      fillOpacity = 1,
+      group = "Field Guides",
+      
+      highlight = highlightOptions(
+        weight = 2,
+        color = "Yellow",
+        fillOpacity = 1,
+        bringToFront = TRUE),
+      
+      label = labels,
+      
+      labelOptions = labelOptions(
+        style = list("font-weight" = "normal", padding = "3px 6px"),
+        textsize = "12px",
+        direction = "auto")) %>% 
+      
+      addLayersControl(
+        baseGroups = c("OSM", "Black & White"),
+        overlayGroups = c("Field Guides"),
+        options = layersControlOptions(collapsed = FALSE)) %>%
+      
+      addLegend(pal = pal, values = ~FG_Numb, title = "Number of Field Guides",
+        position = "bottomleft")
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
